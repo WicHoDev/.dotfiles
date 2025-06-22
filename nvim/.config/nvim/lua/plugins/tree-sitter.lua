3d1ff3e9c91d17eb1705607a8290a5a45652cb8f
WicHo <113863333+WicHoDev@users.noreply.github.com>
2025-06-21 23:33:07 -0500

    update: from WSL

--- nvim/.config/nvim/lua/plugins/tree-sitter.lua
+++ nvim/.config/nvim/lua/plugins/tree-sitter.lua
@@ -1,34 +1,31 @@
 local config = function()
-  require("nvim-treesitter.configs").setup({
-    indent = {
-      enable = true,
-    },
---[[     autotag = {
-      enable = true,
-    },
-  ]]
-    ensure_installed = {
-      "c",
-      "lua",
-      "vim",
-      "vimdoc",
-      "query";
+    require("nvim-treesitter.configs").setup({
+        indent = {  enable = true, },
+        --[[
+    autotag = {  enable = true, },
+    ]]
+        ensure_installed = {
+            "c",
+            "lua",
+            "vim",
+            "vimdoc",
+            "query";
 
-      "cmake",
-      "cpp",
-      "c_sharp",
-      "odin",
-    },
-    auto_install = true,
-    highlight = {
-      enable = true,
-      additiona_vim_regex_highlighting = true,
-    },
-  })
+            "cmake",
+            "cpp",
+            "c_sharp",
+            "odin",
+        },
+        auto_install = true,
+        highlight = {
+            enable = true,
+            additiona_vim_regex_highlighting = true,
+        },
+    })
 end
 
 return {
-  "nvim-treesitter/nvim-treesitter",
-  lazy = false,
-  config = config
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    config = config
 }
