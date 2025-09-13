3fd9583588790babf6cd7b19aff6dceef145809d
WicHo <113863333+WicHoDev@users.noreply.github.com>
2025-09-13 15:17:18 -0500

    Update: Sep-13-2025

---
+++ scripts/.scripts/tmuxSessions.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+tmux new -d -s dev
+tmux new -d -s csci3329 -c "$HOME/Projects/programming/CollegeClasses/CSCI-3329/"
+tmux new -d -s csci3329 -c "$HOME/Projects/programming/CollegeClasses/CSCI-3333/"
